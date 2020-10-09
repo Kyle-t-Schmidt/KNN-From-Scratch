1c419c81efdb23d21383de5c7073b414c3eb75e4
kyle-t-schmidt <kyletschmidt@gmail.com>
2020-10-08 19:53:26 -0700

    finished the data preprocessing method for the test data and changed up the method structure slightly

--- KNN.py
+++ KNN.py
@@ -1,16 +1,18 @@
 import numpy as np
 
-class KNN:
+class knn:
 
 
     def __init__(self):
         self.featureMap = {}
-        self.classifications = []
-        # self.trainData also exists but cannot be defined until the number of
-        # features is known.
+        self.trainClassifications = []
+        self.newClassifications = []
+        self.indexList = []
+        # self.trainData and self.testData also exist but cannot be defined
+        # until after the the number of features is known.
 
 
-    def train(self, TrainDataCsvFile, IndexList):
+    def trainDataProcess(self, TrainDataCsvFile, IndexList):
         """Converts csv file to a Numpy array to be used in KNN Algorithm.
         non-numerical columns, given as a list of indices, are
         one-hot encoded.
@@ -20,6 +22,10 @@ class KNN:
             IndexList (List): A list of indices of columns to be one-hot
             encoded.
         """
+
+
+        self.indexList = IndexList
+
         # Import data as plain text from file, separate each line on comma, and
         # Strip any leading/trailing spaces or /n's.
         data = [line.strip().split(",") for line in open(TrainDataCsvFile)\
@@ -38,8 +44,8 @@ class KNN:
             
             for index, value in enumerate(row):
                 if index == len(row)-1:
-                    self.classifications.append([value])
-                elif index in IndexList:
+                    self.trainClassifications.append([value])
+                elif index in self.indexList:
                     row_cat.append(value)
                 else:
                     row_num.append(value)
@@ -78,17 +84,96 @@ class KNN:
         print('Preprocessing complete')
 
 
-    def Predict(self, K, TestDataCsvFile, includesClassifications=False):
-        """Predicts the classification of each row in the csv file based on the
-        K nearest neighbors. K must be an odd number. If the dataset includes
-        classifications, set includesClassifications to true. The
-        classifications must be the last column in the csv file.
+    def testDataProcess(self, TestDataCsvFile, Classifications=False):
+        """Converts csv file to a Numpy array to be used in KNN Algorithm.
+        Must be in the same format as the train data set. Uses the feature map
+        and non-numerical index list from trainDataProcess.
 
         Args:
-            K (int): Number of nearest neighbors. Must be odd.
             TestDataCsvFile (Str): Path to csv file.
-            includesClassifications (boolean): If the csv file contains 
+            Classifications (boolean): If the csv file contains 
             classifications, set to true.
         """
-        if (K % 2 == 0) or isinstance(K, float):
-            return 'K must be an odd integer'
+
+
+        
+        # Import data as plain text from file, separate each line on comma,
+        #  and strip any leading/trailing spaces or /n's.
+        data = [line.strip().split(",") for line in open(TestDataCsvFile)\
+            .readlines()]
+
+        # Separate the numerical values from the non-numerical values (as
+        # given in the indexList) and separate the classifications into a
+        # third list. Do this by iterating through through each row of the
+        # data and storing the vlaues in their respective list.
+        numerical = []
+        categorical = []
+
+        for row in data:
+            row_num = []
+            row_cat = []
+            
+            # If the test data contains classifications:
+            if Classifications:
+                for index, value in enumerate(row):
+                    if index == len(row)-1:
+                        self.newClassifications.append([value])
+                    elif index in self.indexList:
+                        row_cat.append(value)
+                    else:
+                        row_num.append(value)
+                    
+                numerical.append(row_num)
+                categorical.append(row_cat)
+
+            # If the test data does not contain classifications:
+            else:
+                for index, value in enumerate(row):
+                    if index in self.indexList:
+                        row_cat.append(value)
+                    else:
+                        row_num.append(value)
+                    
+                numerical.append(row_num)
+                categorical.append(row_cat)
+
+        
+        # use the feature map to One-hot encode (make binary) the test 
+        # features.
+        newData = []
+
+        for row in categorical:
+            newRow = []
+            for index, value in enumerate(row):
+                feature = (index, value)
+                if feature not in self.featureMap:
+                    continue
+                newRow.append(self.featureMap[feature])
+            newData.append(newRow)
+
+        # Create a blank numpy array equal to the length of the dataset and
+        #  the width equal to the number of features. Use the feature map
+        # to generate the one-hot encoded data.
+        oneHot = np.zeros((len(newData), len(self.featureMap)))
+
+        for index, row in enumerate(newData):
+            for feature in row:
+                oneHot[index][feature] = 1
+
+        # Append the numerical data that was not one-hot encoded to each line
+        # of the one-hot data. This brings all the data back together.
+        numerical = np.array(numerical, dtype=int)
+        self.testData = np.concatenate((numerical,oneHot), axis=1)
+           
+
+    def predict(K):
+        """Predicts the clasifications of the test dataset using K nearest
+        neighbors with euclidean distance. 
+
+        Args:
+            K (int): number of nearest neighbors to use for classification 
+            prediction
+        """
+
+        # Calculate the euclidean distance of each test point from each train
+        # point.
--- Main.py
+++ Main.py
@@ -1,13 +1,14 @@
-from KNN import KNN
+from KNN import knn
 
 
 trainData = r'/home/kyle/GitHub/KNN-From-Scratch/income.train.txt.5k'
 testData = r'/home/kyle/GitHub/KNN-From-Scratch/income.dev.txt'
 categoricalIndices = [1, 2, 3, 4, 5, 6, 8]
 
-test = KNN()
+test = knn()
 
-test.train(trainData, categoricalIndices)
+test.trainDataProcess(trainData, categoricalIndices)
 print(test.trainData[0])
 
-
+test.testDataProcess(testData, True)
+print(test.testData[0])
